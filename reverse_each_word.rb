03d872b9afdec967b81b77ce7256bd8fcac0879c
Dana Palmer <roiai4@hotmail.com>
2018-07-15 02:31:13 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,16 @@
 require 'pry'
-def reverse_each_word(sentence)
-answer = []
-reversed = sentence.split
+#def reverse_each_word(sentence)
+#answer = []
+#reversed = sentence.split
  #below is the EACH method way of solving the lab
  #reversed.each do |word|
  #answer << word.reverse
 #end
 #binding.pry
 #answer.join(" ")
-reversed.collect { |word| word.reverse }.join(" ")
+#reversed.collect { |word| word.reverse }.join(" ")
+#end
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
