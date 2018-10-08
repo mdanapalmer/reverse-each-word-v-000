50c55b09a47567b585736077bf296dc32aaaf48d
Dana Palmer <roiai4@hotmail.com>
2018-10-08 00:06:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 require 'pry'
 def reverse_each_word(statement)
-  statement.split.collect do |words|
-    words.join(" ").reverse
+  statement.split.collect { |words| words.reverse}.join(" ")
   end
 end
